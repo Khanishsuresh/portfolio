6227dea8707b7adbe11419724e5a2e5a7bd85b49
Khanishsuresh <136162512+Khanishsuresh@users.noreply.github.com>
2024-04-02 10:51:25 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,72 @@
+/*=============== responsive navbar =============== */
+let menu = document.querySelector("#menu-icon");
+let nav = document.querySelector(".navbar-container");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    nav.classList.toggle("active");
+}
+
+
+/*=============== scroll section active link =============== */
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    sections.forEach((section) => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((links) => {
+                links.classList.remove("active");
+                document
+                    .querySelector("header nav a[href*=" + id + "]")
+                    .classList.add("active");
+            });
+        }
+    });
+
+
+
+/*=============== sticky navbar =============== */
+let header = document.querySelector(".header-container");
+
+header.classList.toggle("sticky", window.scrollY > 100);
+
+
+/*=============== remove menu-icon when clicked =============== */
+menu.classList.remove("bx-x");
+nav.classList.remove("active");
+
+};
+
+
+
+/*=============== dark and light mode =============== */
+let darkModeIcon = document.querySelector("#darkmode-icon");
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle("bx-sun");
+    document.body.classList.toggle("darkmode-icon");
+}
+
+
+/*=============== animations =============== */
+ScrollReveal({
+    reset : true,
+    distance : '80px',
+    duration : 2000,
+    delay : 150
+});
+
+ScrollReveal().reveal('.home-content , .heading' , { origin: 'top' });
+ScrollReveal().reveal('.contact form' , { origin: 'bottom' });
+ScrollReveal().reveal('.education-container' , { origin: 'bottom' });
+ScrollReveal().reveal('.skill-container' , { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1 , about-img' , { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content' , { origin: 'right' });
+
+
